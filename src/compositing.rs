999f4bd3ff10685b9711a7fd24c8c6dbbcbfe0c2
Sophie Leochina <87021304+SophieSilver@users.noreply.github.com>
2025-01-11 02:09:20 +0300

    fix: Fixed #1

--- src/compositing.rs
+++ src/compositing.rs
@@ -1,7 +1,9 @@
 use bevy::{
+    ecs::schedule::common_conditions::on_event,
     math::uvec2,
     prelude::*,
     render::camera::{camera_system, ScalingMode, Viewport},
+    window::{PrimaryWindow, WindowResized},
 };
 
 use crate::ui::NonUiArea;
@@ -13,9 +15,12 @@ impl Plugin for CompositingPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, add_viewport_camera).add_systems(
             PostUpdate,
-            resize_viewport
-                .before(camera_system::<OrthographicProjection>)
-                .run_if(resource_changed::<NonUiArea>),
+            (
+                deactivate_camera_on_minimize.run_if(on_event::<WindowResized>),
+                resize_viewport.run_if(resource_changed::<NonUiArea>.or(on_event::<WindowResized>)),
+            )
+                .chain()
+                .before(camera_system::<OrthographicProjection>),
         );
     }
 }
@@ -50,4 +55,27 @@ pub fn resize_viewport(mut camera: Query<&mut Camera, With<ViewportCamera>>, are
         physical_size: area.size().max(uvec2(1, 1)),
         depth: 0.0..1.0,
     })
+}
+
+// Fixes https://github.com/SophieSilver/fractality/issues/1
+pub fn deactivate_camera_on_minimize(
+    mut camera: Query<&mut Camera, With<ViewportCamera>>,
+    window: Query<&Window, With<PrimaryWindow>>,
+) {
+    let Ok(window) = window.get_single() else {
+        return;
+    };
+    let mut camera = camera.single_mut();
+
+    let resolution = window.resolution.physical_size();
+    let minimized = resolution == uvec2(0, 0);
+
+    // checking is_active before setting it avoids tripping change detection
+    if camera.is_active && minimized {
+        debug!("ViewportCamera deactivated");
+        camera.is_active = false;
+    } else if !camera.is_active && !minimized {
+        debug!("ViewportCamera activated");
+        camera.is_active = true;
+    }
 }
